eceea520279a242eb3df617fb89abf95efd11a2b
hosseint <hosseinjealous@gmail.com>
2025-06-15 15:42:19 +0330

    Add new Commit For Automapper

--- webApi/Services/Users/UsersServices.cs
+++ webApi/Services/Users/UsersServices.cs
@@ -1,23 +1,31 @@
 ﻿
 using AutoMapper;
+using System.Linq;
 using webApi.Infrastructure;
 using webApi.Mapper;
+using webApi.Model;
+
 
 namespace webApi.Services.Users
 {
     public class UsersServices : UsersInterface
     {
         public readonly AppDbContext appDbContext;
-        public readonly IMapper AutoMapper;
+        public readonly IMapper autoMapper;
         public UsersServices(AppDbContext appContext, IMapper autoMapper)
         {
             this.appDbContext = appContext;
-            AutoMapper = autoMapper;
+            this.autoMapper = autoMapper;
         }
 
         public void AddNewUsers(UsersDTO userDTO)
         {
-            throw new NotImplementedException();
+            var users = appDbContext.usersSample.FirstOrDefault(c => c.Id == userDTO.Id);
+            if (users != null)
+                throw new Exception("the user you want to add exist");
+            var convertedUsers = autoMapper.Map<Model.Users>(userDTO);
+            appDbContext.usersSample.Add(convertedUsers);
+            appDbContext.SaveChanges();          
         }
 
         public string DeleteUsers(int id)
@@ -27,12 +35,16 @@ namespace webApi.Services.Users
 
         public List<UsersDTO> GetAllUsers()
         {
-            
+            throw new NotImplementedException();
         }
 
         public UsersDTO GetUser(int id)
         {
-            throw new NotImplementedException();
+            var users = appDbContext.usersSample.FirstOrDefault(c=>c.Id == id);
+            if (users == null)
+                throw new Exception("the user not found");
+            var convertedUsers = autoMapper.Map<UsersDTO>(users);
+            return convertedUsers;
         }
 
         public string UpdateUsers(int id)
