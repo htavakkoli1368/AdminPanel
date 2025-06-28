e5b48396f50d8e04371f94127c5b557027542ef1
hosseint <hosseinjealous@gmail.com>
2025-06-28 11:26:45 +0330

    add a basic authentication with jwt and some static user

--- webApi/Controllers/AuthenticationController.cs
+++ webApi/Controllers/AuthenticationController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace webApi.Controllers
 {
@@ -7,9 +12,15 @@ namespace webApi.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        private readonly IConfiguration _config;
+
+        public AuthenticationController(IConfiguration config)
+        {
+            _config = config;
+        }
         public record AuthenticationData(string? UserName , string? Password);
         public record UserData(int Id , string? UserName);
-        [HttpPost("Token")] 
+        [HttpPost("Token")]        
          public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
         {
             var user = ValidateCredential(data);
@@ -17,13 +28,42 @@ namespace webApi.Controllers
             {
                 return Unauthorized();
             }
-            return "token";
+            var token = GenerateToken(user);
+            return Ok(token);
+        }
+        private string GenerateToken(UserData user)
+        {   
+            //first create secreteKey
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetValue<string>("Authentication:SecretKey")));
+
+            //singing credential in order to proof and sign our token.it takes 2 item.first one is secretkey that we create
+            //last step and choose our securityalgoritm that we choose hmacSha256
+            var signingCredential = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            List <Claim> claims = new();
+            claims.Add(new(JwtRegisteredClaimNames.Sub,user.Id.ToString()));
+            claims.Add(new(JwtRegisteredClaimNames.UniqueName,user.UserName));
+
+            var token = new JwtSecurityToken(
+                 _config.GetValue<string>("Authentication:Issuer"),
+                 _config.GetValue<string>("Authentication:Audience"),
+                 claims,
+                 DateTime.UtcNow,
+                 DateTime.UtcNow.AddSeconds(30),
+                 signingCredential
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
         private UserData ValidateCredential(AuthenticationData data) 
         {
             if(CompareValues(data.UserName,"ht") && CompareValues(data.Password,"858585"))
             {
                 return new UserData(1, data.UserName );
+            }
+            if(CompareValues(data.UserName,"hossein") && CompareValues(data.Password,"696969"))
+            {
+                return new UserData(2, data.UserName );
             }
             return null; 
         }
