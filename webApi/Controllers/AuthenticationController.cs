8295989f4561ad4914b7e0bee4c08bebd24e3b89
hosseint <hosseinjealous@gmail.com>
2025-06-28 09:52:42 +0330

    add a basic authentication to project in order to prepare for JWT(json web token)

---
+++ webApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public record AuthenticationData(string? UserName , string? Password);
+        public record UserData(int Id , string? UserName);
+        [HttpPost("Token")] 
+         public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
+        {
+            var user = ValidateCredential(data);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+            return "token";
+        }
+        private UserData ValidateCredential(AuthenticationData data) 
+        {
+            if(CompareValues(data.UserName,"ht") && CompareValues(data.Password,"858585"))
+            {
+                return new UserData(1, data.UserName );
+            }
+            return null; 
+        }
+
+        private bool CompareValues(string? actual, string? expected)
+        {
+            if(actual is not null)
+            {
+               if(actual.Equals(expected))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+    }
+}
