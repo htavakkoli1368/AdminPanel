1e1f47cfab6807c43e2771444590b497f788d5f0
hosseint <hosseinjealous@gmail.com>
2025-07-08 15:24:28 +0330

    add to userModel some validation that we apply without any nuget.

--- webApi/Model/UsersModel.cs
+++ webApi/Model/UsersModel.cs
@@ -1,16 +1,25 @@
-﻿namespace webApi.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webApi.Model
 {
     public class UsersModel
     {
 
+        [Required]
+        [Range(1, int.MaxValue)]        
         public int Id { get; set; }
-
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
         public string UserName { get; set; }
-
+        [Required]
+        [MinLength(7)]
         public string Password { get; set; }
 
         public string Role { get; set; }
         public bool IsAdmin { get; set; }
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$",ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
 
     }
 
