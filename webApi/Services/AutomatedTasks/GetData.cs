fb817cceeba7758fff2a7b3f808adfe1cd5c8986
hosseint <hosseinjealous@gmail.com>
2025-07-15 14:38:08 +0330

    Add automated task to the project for example get data or running a web service

---
+++ webApi/Services/AutomatedTasks/GetData.cs
@@ -0,0 +1,12 @@
+﻿using Quartz;
+
+namespace webApi.Services.AutomatedTasks
+{
+    public class GetData : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
