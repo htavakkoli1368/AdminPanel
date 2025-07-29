48f14391b6f8bb03296f3b904703d46a843ce008
htavakkoli1368 <32728223+htavakkoli1368@users.noreply.github.com>
2025-07-29 22:13:40 +0330

    Add ToDo Project To the solution

---
+++ ToDoApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualBasic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public record AuthenticationData(string UserName,string PassWord);
+        public record UserData(int Id , string FirstName, string LastName, string UserName);
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult<string> Authenticate([FromBody]AuthenticationData data)
+        {
+            var user = ValidateCredential(data);
+            if (user == null) 
+            {
+                return Unauthorized();
+            }
+
+            string token = GenerateToken(user);
+            return Ok(token);
+        }
+
+        private string GenerateToken(UserData user)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetValue<string>("Authentication:SecretKey")));
+
+            var signinCredential = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            List<Claim> claims = new();
+
+            claims.Add(new(JwtRegisteredClaimNames.Sub,user.Id.ToString()));
+            claims.Add(new(JwtRegisteredClaimNames.UniqueName,user.UserName));
+            claims.Add(new(JwtRegisteredClaimNames.GivenName,user.FirstName));
+            claims.Add(new(JwtRegisteredClaimNames.FamilyName,user.LastName));
+
+            var token = new JwtSecurityToken(
+                 _config.GetValue<string>("Authentication:Issuer"),
+                 _config.GetValue<string>("Authentication:Audience"),
+                 claims,
+                 DateTime.UtcNow,
+                 DateTime.UtcNow.AddMinutes(1),
+                 signinCredential
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserData? ValidateCredential(AuthenticationData data)
+        {
+           if(CompareValue(data.UserName,"hossein") && CompareValue(data.PassWord, "ht123"))
+            {
+                return new UserData(Id: 1, FirstName: "hossein", LastName: "tavakkoli", UserName: data.UserName);
+            }
+            if (CompareValue(data.UserName, "jalal") && CompareValue(data.PassWord, "jalal1370"))
+            {
+                return new UserData(Id: 2, FirstName: "jalal", LastName: "tavakkoli", UserName: data.UserName);
+            }
+            return null;
+        }
+        private bool CompareValue(string actual , string expected)
+        {
+            if(actual is not null)
+            {
+                if(actual.Equals(expected))
+                {
+                    return true;
+                }
+            }
+            return false;
+             
+        }
+    }
+}
--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,4 +1,10 @@
 
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
 namespace ToDoApi
 {
     public class Program
@@ -13,6 +19,28 @@ namespace ToDoApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthorization(opt=>
+            {
+                opt.FallbackPolicy = new AuthorizationPolicyBuilder()
+                   .RequireAuthenticatedUser()
+                   .Build();
+            }
+            );
+
+            builder.Services.AddAuthentication("Bearer").AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+                    ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+                };
+            });
+            // builder.Services.AddDbContext<AppDbContext>(p => p.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
             var app = builder.Build();
 
@@ -24,6 +52,7 @@ namespace ToDoApi
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
